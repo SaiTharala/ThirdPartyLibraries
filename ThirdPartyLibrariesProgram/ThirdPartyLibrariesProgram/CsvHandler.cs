6af44f632e8214505528f3910c75335fce7d3934
unknown <Saitharala@gmail.com>
2021-12-09 00:15:53 +0530

    [Tharala Sai Prashanth] Added CSV file in to String Array Feauture

---
+++ ThirdPartyLibrariesProgram/ThirdPartyLibrariesProgram/CsvHandler.cs
@@ -0,0 +1,35 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ThirdPartyLibrariesProgram
+{
+    class CsvHandler
+    {
+        public void ImplementCsvHandling()
+        {
+            string importFilePath = @"E:\GIT BL\CSV Files\ThirdPartyLibraries\ThirdPartyLibrariesProgram\ThirdPartyLibrariesProgram\Utility\Address.csv";
+            string exportFilePath = @"E:\GIT BL\CSV Files\ThirdPartyLibraries\ThirdPartyLibrariesProgram\ThirdPartyLibrariesProgram\Utility\export.csv";
+            using (var reader = new StreamReader(importFilePath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<AddressData>().ToList();
+                Console.WriteLine("read successfully\n");
+                foreach (AddressData addressData in records)
+                {
+                    Console.Write("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\n", addressData.FirstName, addressData.LastName, addressData.Address, addressData.City, addressData.State, addressData.Code);
+                }
+                Console.WriteLine("\n Now reading and Writing to csv file");
+                using (var writer = new StreamWriter(exportFilePath))
+                using (var csvExport = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csvExport.WriteRecords(records);
+                }
+            }
+        }
+    }
+}
---
+++ ThirdPartyLibrariesProgram/ThirdPartyLibrariesProgram/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ThirdPartyLibrariesProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            CsvHandler csv = new CsvHandler();
+            csv.ImplementCsvHandling();
+        }
+    }
+}
